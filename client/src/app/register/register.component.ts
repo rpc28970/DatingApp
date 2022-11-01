fc789c094c46dc4ecb01b382e8db8e79cc8cd90a
rpc28970 <rpc28970@gmail.com>
2022-11-01 10:04:59 -0400

    Created New Components, Added Auth Routing Guard,  added new themes and styles

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -10,7 +11,10 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor(private accountService: AccountService) { }
+  constructor(
+    private accountService: AccountService,
+    private toastr: ToastrService
+    ) { }
 
   ngOnInit(): void {
   }
@@ -18,7 +22,7 @@ export class RegisterComponent implements OnInit {
   register() {
     this.accountService.register(this.model).subscribe({
       next: response => {console.log(response); this.cancel();},
-      error: error => console.log(error)
+      error: error => {console.log(error); this.toastr.error(error.error)}
     })
   }
 
